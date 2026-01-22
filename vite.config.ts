07d5bad43c360be6a992a642d2d6b929a4ee4eb0
DeiversonMAP <73710805+DeiversonMAP@users.noreply.github.com>
2026-01-22 18:43:56 -0300

    fix: update project name in README and Vite config to reflect new version

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/ufjf-dcc207-2025-3-a-sem2-deiverson",
+    base: "/ufjf-dcc207-2025-3-a-sem03-giant",
     plugins: [react()],
 });
